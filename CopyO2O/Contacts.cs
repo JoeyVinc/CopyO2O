a27b2edaa5ec70b183445349ed84c234dea95460
Johannes Krieg <johannes.krieg@outlook.de>
2019-02-23 15:17:37 +0100

    -

---
+++ CopyO2O/Contacts.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyO2O
+{
+    public struct Address
+    {
+        public string Street;
+        public string Number;
+        public string City;
+        public string Zip;
+        public string Country;
+    }
+
+    public struct Category
+    {
+        public string Name;
+        public System.Drawing.Color Color;
+    }
+
+    public struct EMail
+    {
+        public string Address;
+        public string Title;
+    }
+
+    public struct ContactType
+    {
+        public string SaveAs;
+        public string DisplayName;
+        public string GivenName;
+        public string Surname;
+        public string Company;
+        public DateTime? Birthday;
+        public DateTime? AnniversaryDay;
+        public Boolean VIP;
+        public List<Category> Categories;
+        public string Notes;
+
+        public Address PrivateLocation;
+        public EMail PrivateMailAddress;
+        public string PrivatePhoneNumber;
+        public string PrivateMobileNumber;
+        public string PrivateFaxNumber;
+
+        public Address BusinessLocation;
+        public EMail BusinessMailAddress;
+        public string BusinessPhoneNumber;
+        public string BusinessMobileNumber;
+        public string BusinessFaxNumber;
+
+        public string PictureTmpFilename;
+    }
+
+    public class ContactsType : List<ContactType> { }
+}
